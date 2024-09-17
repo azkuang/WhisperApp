579019c62bd6816d8a609ac90f24efe671b2c3ff
unknown <alexzkuang0314@gmail.com>
2024-09-17 00:29:18 -0400

    Added delete button and edit button

--- frontend/src/components/NoteDetails.js
+++ frontend/src/components/NoteDetails.js
@@ -1,9 +1,28 @@
+import { useNoteContext } from "../hooks/useNotesContext";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 const NoteDetails = ({ note }) => {
+
+    const { dispatch } = useNoteContext();
+
+    const handleClick = async () => {
+        const response = await fetch('/api/notes/' + note._id, {
+            method: 'DELETE'
+        });
+
+        const json = await response.json();
+
+        if (response.ok) {
+            dispatch({ type: 'DELETE_NOTE', payload: json });
+        }
+    };
+
     return (
         <div className="note-details">
             <h4>{note.title}</h4>
-            <p><strong>Contents: </strong>{note.contents}</p>
-            <p>{note.createdAt}</p>
+            <h5>{note.contents}</h5>
+            <p>{formatDistanceToNow(new Date(note.createdAt), { addSuffix: true })}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+            <button className="material-symbols-outlined">edit</button>
         </div>
     )
 }
