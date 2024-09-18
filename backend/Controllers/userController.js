998d8849e88b824e808e99857e82e04f39b64932
unknown <alexzkuang0314@gmail.com>
2024-09-17 23:48:02 -0400

    JWT Token functions

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,6 +1,11 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
+// Creates JWT Token that lasts 3 days
+const createToken = (_id) => {
+    return jwt.sign({ _id: _id}, process.env.JWT_SECRET, { expiresIn: '3d' });
+};
+
 // login user
 const loginUser = async (req, res) => {
     res.status(200).json({ message: 'login user' });
@@ -13,7 +18,10 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password);
 
-        res.status(200).json({ email, user });
+        // Create a token
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
