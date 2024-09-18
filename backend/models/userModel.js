f30f3747172c47c07fa02cd6b925f66395c3aff8
unknown <alexzkuang0314@gmail.com>
2024-09-17 23:40:23 -0400

    Added login and signup functions

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,37 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+// Static signup method
+userSchema.statics.signup = async function(email, password) {
+    
+    // validate email and password
+    if (!email || !password) {
+        throw Error('All fields must be filled.');
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Not a valid email.');
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password too weak, please include at least 8 characters, one captial letter, one number, and one special character.');
+    }
+    
+    const existsEmail = await this.findOne({ email });
+
+    if (existsEmail) {
+        throw Error('Email already in use.');
+    }
+
+    // Generate salt for passwords
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash passwords
+    const hash = await bcrypt.hash(password, salt);
+
+    // Store email + password in DB
+    const user = await this.create({ email: email, password: hash });
+ 
+    return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
