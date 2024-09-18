b3631922569cf13572a9f949bf577226a1db284d
unknown <alexzkuang0314@gmail.com>
2024-09-18 00:14:29 -0400

    Login functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -48,4 +48,26 @@ userSchema.statics.signup = async function(email, password) {
     return user;
 };
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+    // validate email and password
+    if (!email || !password) {
+        throw Error('All fields must be filled.');
+    }
+
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error('Incorrect login');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw Error('Incorrect login');
+    }
+
+    return user;
+};
+
 module.exports = mongoose.model('User', userSchema);
